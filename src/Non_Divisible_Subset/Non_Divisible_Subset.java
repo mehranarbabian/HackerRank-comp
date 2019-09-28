a3ef84fdbffb7485ef2352425a83da90dcb7e83d
mehranarbabian <mehranarbabian76@gmail.com>
2019-09-29 00:25:27 +0330

    1/1

--- src/Non_Divisible_Subset/Non_Divisible_Subset.java
+++ src/Non_Divisible_Subset/Non_Divisible_Subset.java
@@ -1,6 +1,7 @@
 package Non_Divisible_Subset;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,35 +11,44 @@ public class Non_Divisible_Subset {
         int n=scanner.nextInt();
         int k=scanner.nextInt();
         int [] input_set=new int[n];
-        List<Integer> subset=new ArrayList<>();
-        List<Integer> temp=new ArrayList<>();
+        List<Integer> list_of_numbers_for_start_creating_subset=new ArrayList<>();
+        List<Integer> list_of_results=new ArrayList<>();
+
         for (int i=0;i<n;++i)
             input_set[i]=scanner.nextInt();
 
-temp.add(input_set[0]);
-        for (int a=0;a<get_size_of_temp(temp);++a) {
-            subset.add(temp.get(0));
-            temp.remove(0);
+list_of_numbers_for_start_creating_subset.add(input_set[0]);
+        for (int a = 0; a<3; ++a) {
+            List<Integer> subset=new ArrayList<>();
+            if (list_of_numbers_for_start_creating_subset.size()>0) {
+                subset.add(list_of_numbers_for_start_creating_subset.get(0));
+                list_of_numbers_for_start_creating_subset.remove(0);
+            }
             for (int i = 1; i < input_set.length; ++i) {
                 int counter = 0;
-                for (int j = 0; j < get_size_of_subset(subset); ++j)
+                for (int j = 0; j < get_size_of_input(subset); ++j)
                     if ((input_set[i] + subset.get(j)) % k != 0 && input_set[i] != subset.get(j))
                         ++counter;
 
-                if (counter == get_size_of_subset(subset))
+                if (counter == get_size_of_input(subset))
                     subset.add(input_set[i]);
-                else temp.add(input_set[i]);
+                else list_of_numbers_for_start_creating_subset.add(input_set[i]);
             }
+list_of_results.add(subset.size());
         }
-        System.out.println(subset.size());
 
+        int max=list_of_results.get(0);
+        if (list_of_results.size()>1)
+            for (Integer list_of_result : list_of_results) {
+                if (list_of_result>max)
+                    max=list_of_result;
+
+            }
+        System.out.println(max);
     }
-    public static int get_size_of_subset(List<Integer> subset)
+    public static int get_size_of_input(List<Integer> input)
     {
-        return subset.size();
+        return input.size();
     }
-    public static int get_size_of_temp(List<Integer> temp)
-    {
-        return temp.size();
-    }
+
 }
